4476270c70ce0de7f7e0ede5c2012c44bd6429c7
surfacing8671 <jaeweb8671@gmail.com>
2024-06-13 16:00:18 -0500

    remove images

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e16004040f9d3d5f"; }
+/******/ 		__webpack_require__.h = function() { return "2907de803f84600a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
